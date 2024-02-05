8719d36a24a885e5072b85df418facfa22f890e9
shaikat17 <shaikatpal56@gmail.com>
2024-02-06 00:08:04 +0600

    user interface and user model added

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    needsPasswordChnage: {
+        type: Boolean,
+        default: true,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "student", "faculty"],
+    },
+    status: {
+        type: String,
+        enum: ["in-progress", "blocked"],
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+},
+
+    {
+        timestamps: true
+    }
+
+)
+
+export const User = model<TUser>('User', userSchema)
